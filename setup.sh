ecd9cbdf49d5077e02349cdc4aa55260e680a98f
Nguyen Hieu (iteratec) <hinguyen@lxn-dev-app00.bmd.local>
2016-12-27 15:42:44 +0100

    remove setup for YCM

--- setup.sh
+++ setup.sh
@@ -8,8 +8,3 @@
 # won't work if Vundle is installed in bundle
 # it must be installed in .vim directory
 vim +PluginInstall +qall
-
-# compile YCM
-cd ~/.vim/bundle/YouCompleteMe
-python ./install.py
-cd -
