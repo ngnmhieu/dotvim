b3ca0cc4f7e5a0c11060f90eabf10f4e1941ef7b
Hieu Nguyen <ngnmhieu@gmail.com>
2018-11-28 23:38:16 +0100

    TMUX configuration for mac and linux; setup.sh script for tmux

---
+++ dotfiles/tmux/setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+## USAGE:
+##   setup.sh [-y] 
+## PARAMETERS:
+##   -y   confirm all promts
+
+PLATFORM=$(uname)
+[[ "$1" == "-y" || "$1" == "-Y" ]] && CONFIRM_ALL=1 || CONFIRM_ALL=0
+CONF_PATH=$HOME/.vim/dotfiles/tmux/tmux.conf
+LINK_PATH=$HOME/.tmux.conf
+BACKUP_FILEPATH=$HOME/.tmux.conf.bak
+SUCCESS=1
+
+# Program installation helper
+install_program () {
+  if [[ $PLATFORM == "Darwin" ]]; then 
+    INSTALL_COMMAND="brew install"
+  else
+    INSTALL_COMMAND="sudo apt install"
+  fi
+
+  $INSTALL_COMMAND $1
+}
+
+check_command_existance () {
+  PROGRAM=$1
+  command -v $PROGRAM > /dev/null
+  if [[ $? != 0 ]]; then
+    if [[ $CONFIRM_ALL == 1 ]]; then
+      INSTALL=Y
+    else
+      read -p "'$PROGRAM' is required for copying to clipboard to work. Do you want to install it? (Y/N): " INSTALL 
+    fi
+
+    if [[ $INSTALL == [yY] || $INSTALL == [yY][eE][sS] ]]; then
+      echo "Installing $PROGRAM ..."
+      install_program $PROGRAM
+    else
+      echo "Skipping installation of '$PROGRAM'"
+    fi
+  fi
+}
+
+echo "Configuring TMUX ..."
+
+if [[ -L "$LINK_PATH" ]]; then
+  if [[ $CONFIRM_ALL == 1 ]]; then
+    OVERWRITE=Y
+  else
+    read -p "A configuration file already existed at $LINK_PATH. Overwrite? (Y/N): " OVERWRITE
+  fi
+
+  if [[ $OVERWRITE == [yY] || $OVERWRITE == [yY][eE][sS] ]]; then
+    rm $LINK_PATH
+  else
+    echo "Configuration aborted"
+    exit 1;
+  fi
+fi
+
+echo "Creating soft link ..."
+ln -s $CONF_PATH $LINK_PATH
+
+echo "Checking dependencies ..."
+
+if [[ $PLATFORM == "Darwin" ]]; then 
+  check_command_existance reattach-to-user-namespacex
+else
+  check_command_existance xclip 
+fi
+
+echo "Configuration completed!"
